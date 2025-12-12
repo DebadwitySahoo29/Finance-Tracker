5878e52fd428bba97451ebbf198e061a45aeedae
DebadwitySahoo29 <sahoodebadwity@gmail.com>
2025-12-12 13:02:21 +0530

    Update api.js

--- frontend/finance-tracker-frontend/src/services/api.js
+++ frontend/finance-tracker-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for your backend
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://finance-tracker-backend-hc34.onrender.com';
 
 // Create axios instance
 const api = axios.create({
